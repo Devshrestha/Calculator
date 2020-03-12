72d2c173e30cc0ea5ae7519a0dbd011482e22037
Devansh shrestha <shrestha71devansh@gmail.com>
2020-03-12 18:26:43 +0530

    adding the code file containing operators

---
+++ code/main.py
@@ -0,0 +1,42 @@
+import  math
+
+class operators:
+    
+    def add(self,x,y):
+        return x+y
+
+    def sub(self,x,y):
+        return x-y
+    
+    def multi(self,x,y):
+        return x*y
+    
+    def mod(self,x,y):
+        return x%y
+
+    def div(self,x,y):
+        return x/y
+
+    def power(self,x,y):
+        return x**y
+
+    def square(self,x):
+        return x*x
+
+    def cube(self,x):
+        return x**3
+
+    def sqrt(self,x):
+        return x**(0.5)
+
+    def cubrt(self,x):
+        return x*(1/3)
+
+    def sine(self,x):
+        return math.sin(x)
+        
+    def cosine(self,x):
+        return math.cos(x)
+
+    def tangent(self,x):
+        return math.tan(x)    
